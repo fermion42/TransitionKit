94aa62e6d6df7f96d1408b1fd1f341ce3bff3751
Gulakya Young <glaxxxya@gmail.com>
2015-11-30 17:51:30 +0800

    Add 'Carthage' Supports

---
+++ TransitionKit/TransitionKit.h
@@ -0,0 +1,27 @@
+//
+//  TransitionKit.h
+//  TransitionKit
+//
+//  Created by Yang Han on 15/11/30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+
+    #import <Availability.h>
+    #if __MAC_OS_X_VERSION_MIN_REQUIRED
+        #import <Cocoa/Cocoa.h>
+    #endif
+#endif
+
+//! Project version number for TransitionKit.
+FOUNDATION_EXPORT double TransitionKitVersionNumber;
+
+//! Project version string for TransitionKit.
+FOUNDATION_EXPORT const unsigned char TransitionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionKit/PublicHeader.h>
+
